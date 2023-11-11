2819ca2920d411dfdc7397394b5fbe563749d1df
evgeny <tea985@yandex.ru>
2023-11-12 02:41:33 +0300

    ТЗ 13 Первая итерация

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,39 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUserById(@PathVariable Integer id) {
+        return userService.findUser(id);
+    }
+
+    @PostMapping
+    public UserDto saveNewUser(@RequestBody UserDto user) {
+        return userService.createUser(user);
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto updateUser(@PathVariable Integer id, @RequestBody UserDto user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable Integer userId) {
+        userService.deleteUser(userId);
+    }
 }
