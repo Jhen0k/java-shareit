a3752ef33c9e1af976ec6a55406be66d92f88ae6
Jhen0k <93070923+Jhen0k@users.noreply.github.com>
2023-12-02 00:23:13 +0300

    Update BookingServiceImpl.java

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -12,8 +12,6 @@ import ru.practicum.shareit.item.ItemRepository;
 import ru.practicum.shareit.item.ItemService;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.mappers.BookingMapper;
-import ru.practicum.shareit.mappers.ItemMapper;
-import ru.practicum.shareit.mappers.UserMapper;
 import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
